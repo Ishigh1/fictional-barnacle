44924215f701c54c6741ae11204c1325822c92e3
Ishigh1 <45936615+Ishigh1@users.noreply.github.com>
2019-01-29 15:21:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -385,8 +385,9 @@ client.on('message', msg => {
 			})
 		}
 		else if (msg.content.indexOf("!birthday") != -1) {
-			var date = scanf("!birthday %d/%d/%d");
-			console.log(date);
+			//var date = scanf("!birthday %d/%d/%d");
+			//console.log(date);
+			console.log(scanf("!birthday %d/%d/%d"));
 			//msg.channel.send(showdate(date));
 		}
 	}
